2115506770e020df939a883ce949cd45398d42cf
Bea Obiero <bea.obiero@gmail.comgit config --global user.email bea.obiero@gmail.comgit config --global user.email + + bea.obiero@gmail.com>
2024-01-04 22:20:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+// a) function to generate thankyou message
+function writeCards(names, occassion) {
+    const thankYouMessages = [];
+
+    for(const name of names) {
+        const message = `Thankyou, ${name} for the wonderful ${occasion} gift!`;
+        thankYouMessages.push(message);
+    }
+
+    return thankYouMessages;
+}
+
+// b)function to countdown numbers
+
+function countDown(number) {
+    for (let i = number; i > 0; i --){
+        console.log(i);
+    }
+}
+
+const names = ['Guadalupe', 'Ollie', 'Aki'];
+const occasion = "surprise";
+const thankYouMessages = writeCards(names,occassion);
+console.log(thankYouMessages);
+
+const countdownNumber = 5;
+countDown(countdownNumber);
